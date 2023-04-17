1d4ef77d93a1ede92b0d8f899885185f17e1def8
nguyentrongbut <115614825+nguyentrongbut@users.noreply.github.com>
2023-04-17 10:56:51 +0700

    commit

--- src/Account.java
+++ src/Account.java
@@ -2,4 +2,22 @@ public class Account {
     private String customerCode;
     private int accNumber;
     private long amount;
+    public Account(String customerCode, int accNumber) {
+        this.customerCode = customerCode;
+        this.accNumber = accNumber;
+        this.amount = 0;
+    }
+
+    public String getCustomerCode() {
+        return customerCode;
+    }
+
+    public int getAccNumber() {
+        return accNumber;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // Prompt user to input account details
+        String customerCode;
+        do {
+            System.out.print("Enter customer code: ");
+            customerCode = sc.nextLine();
+        } while (customerCode.length() != 5);
+
+        int accNumber;
+        do {
+            System.out.print("Enter account number: ");
+            accNumber = sc.nextInt();
+        } while (accNumber < 100000 || accNumber > 999999 || accNumber / 100000 != 100);
+
+        // Create account object
+        Account account = new Account(customerCode, accNumber);
+        System.out.println("Account created successfully!");
     }
 }
